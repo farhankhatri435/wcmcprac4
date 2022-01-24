338261d8457ada06285b150cb6711900e92ac7a0
farhankhatri435 <19it055@charusat.edu.in>
2022-01-25 00:41:10 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,79 @@
-//@dart=2.9
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
+  _HomeState createState() => _HomeState();
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int Datechosen = 0, Monthchosen = 0, Yearchosen = 0;
-  String year = '', month = '', days = '';
-
+class _HomeState extends State<MyApp> {
+  TextEditingController num1 = TextEditingController();
+  TextEditingController num2 = TextEditingController();
+  String result = '0';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+        title: Text('PRACTICAL_3'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[600],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                pickBdate();
-              },
-              child: Text('Pick a Date'),
+            // first input field
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  //labelText: 'Enter 1st number',
+                  hintText: 'Enter 1st number',
+                ),
+                keyboardType: TextInputType.number,
+                controller: num1,
+              ),
             ),
-            Text(year + ' ' + month + ' ' + days + ' '),
+            //second input filed
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  //labelText: 'Enter 2nd number',
+                  hintText: 'Enter 2nd number',
+                ),
+                keyboardType: TextInputType.number,
+                controller: num2,
+              ),
+            ),
+            ElevatedButton(
+              child: Text('ADD'),
+              onPressed: () {
+                setState(() {
+                  int sum = int.parse(num1.text) + int.parse(num2.text);
+                  result = sum.toString();
+                });
+              },
+            ),
+            SizedBox(height: 30),
+            Text(
+              result,
+              style: TextStyle(
+                color: Colors.yellow,
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
           ],
         ),
       ),
     );
-  }
-
-  Future pickBdate() async {
-    var date = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(1900),
-      lastDate: DateTime.now(),
-    );
-
-    Datechosen = int.parse(DateFormat("dd").format(date));
-    Monthchosen = int.parse(DateFormat("MM").format(date));
-    Yearchosen = int.parse(DateFormat("yyyy").format(date));
-
-    int currentDate = int.parse(DateFormat("dd").format(DateTime.now()));
-    int currentMonth = int.parse(DateFormat("MM").format(DateTime.now()));
-    int currentYear = int.parse(DateFormat("yyyy").format(DateTime.now()));
-
-    int numberOfDays = findDays(currentMonth, currentYear);
-
-    setState(() {
-      if (currentDate - Datechosen >= 0) {
-        days = (currentDate - Datechosen).toString() + " days";
-      } else {
-        days = (currentDate + numberOfDays - Datechosen).toString() + " days";
-        currentMonth--;
-      }
-
-      if (currentMonth - Monthchosen >= 0) {
-        month = (currentMonth - Monthchosen).toString() + " months";
-      } else {
-        month = (currentMonth + 12 - Monthchosen).toString() + " months";
-        currentYear--;
-      }
-
-      year = (currentYear - Yearchosen).toString() + " years";
-    });
-  }
-
-  int findDays(int currentMonth, int currentYear) {
-    if (currentMonth == 1 ||
-        currentMonth == 3 ||
-        currentMonth == 5 ||
-        currentMonth == 7 ||
-        currentMonth == 8 ||
-        currentMonth == 10 ||
-        currentMonth == 12) {
-      return 31;
-    } else if (currentMonth == 4 ||
-        currentMonth == 6 ||
-        currentMonth == 9 ||
-        currentMonth == 11) {
-      return 30;
-    } else {
-      if (checkYear(currentYear)) {
-        return 29;
-      } else {
-        return 28;
-      }
-    }
-  }
-
-  bool checkYear(int year) {
-    if (year % 400 == 0) return true;
-
-    if (year % 100 == 0) return false;
-
-    if (year % 4 == 0) return true;
-    return false;
   }
 }
